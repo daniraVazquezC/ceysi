0cae6a484e3580b4a7d9f140098649ffa8a8746b
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-03 23:24:41 -0600

    vista de inicio

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+  # Esta linea indica que antes de ejecutar cualquier metodo de este controlador se debe verificar que sea un usuario autenticado
+  before_action :authenticate_user!
+  # Indico el layout a utilizar para las vistas de este controlador
+  layout 'general'
+
+  def index
+  end 
+
 end

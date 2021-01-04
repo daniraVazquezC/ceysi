2e7ffd5db110f9cca5ae859bf38fb8b9a02a2a5b
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-04 17:13:53 -0600

    se agregó codigo de ejemplo a algunas vistas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # : :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
 
   # modulos de devise utilizados en el sistema
   # database_authenticatable: almacena una contraseña en la bd para validad el inicio de sesión
   # recoverable: Permite al usuario restablecer su contraseña
   # rememberable: recordar al usuario de una cookie
+  # invitable: modulo agregado con una gema extra que permite a los usuarios registrados "invitar" personas al sistema
   devise :invitable, :database_authenticatable, :recoverable, :rememberable
 end

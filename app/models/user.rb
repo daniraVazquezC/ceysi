5ab639a14421850ae50a8fc38dcb963209bba6af
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-05 20:46:23 -0600

    se agregan validaciones y regla de autenticacion de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
-
+  # Comentario: 
   # modulos de devise utilizados en el sistema
   # database_authenticatable: almacena una contraseña en la bd para validad el inicio de sesión
   # recoverable: Permite al usuario restablecer su contraseña
   # rememberable: recordar al usuario de una cookie
   # invitable: modulo agregado con una gema extra que permite a los usuarios registrados "invitar" personas al sistema
   devise :invitable, :database_authenticatable, :recoverable, :rememberable
+
+  # Comentario: Realiza la validación de que el correo electronico este presente antes de crear el registro en la base de datos 
+  validates :email, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
+  # Comentario: Si el nuevo usuario no cuenta con un nombre se le asignará uno por defecto
+  before_save :set_name
+
+  # Comentario: Este es el método que se llama en el before_save(antes de guardar)
+  def set_name
+    self.name = "Usuario del sistema" if self.name.blank?
+  end
+
+  # Comentario: Este método indica si la cuenta del usuario esta o no activa para que inicie sesión en el sistema
+  def active_for_authentication?
+    super && self.active
+  end
+  
 end

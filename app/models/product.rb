1c9fe35c2d1d87e8dd816a55d1fe278bf52d6058
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-06 22:36:00 -0600

    se modifica el modelo del producto asi como sus vistas

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
+  # Comentario: Si el campo de minimum_stock va con un valor nulo (nil) se le asigna el valor de 0 por defecto
+  before_save :set_minimum_stock
 
+  # Comentario: Este es el método que se llama en el before_save(antes de guardar)
+  def set_minimum_stock
+    self.minimum_stock = 0 if self.minimum_stock.nil?
+  end
+  
 end

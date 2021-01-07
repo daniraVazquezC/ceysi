c1b8e120886870b1946ea3296df6acfa3b8aa4bb
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-07 14:37:29 -0600

    se crea el controlador para las transacciones asi como para cada uno de sus tipos: compras y ventas

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,34 @@
+class PurchasesController < ApplicationController
+  # Comentario: Esta linea indica que antes de ejecutar cualquier metodo de este controlador se debe verificar que sea un usuario autenticado
+  before_action :authenticate_user!
+  # Comentario: Indica el layout a utilizar para las vistas de este controlador
+  layout 'general'
+
+  before_action :set_transaction, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @purchase = Purchase.all
+  end
+
+  def show
+  end
+
+  def new
+    
+  end
+
+  def edit
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def transaction_params
+      params.fetch(:transaction, {})
+    end
+end

83ef6b4d11baa4833bd520d22e97e2c7a28774fa
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-08 08:34:10 -0600

    se modifica el modelo de transaction y el de transaction_details

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,19 @@
 class Transaction < ApplicationRecord
-  # Comentario: Esto indica una relación con los usuarios en donde se indica que una "transaction"
+  # Comentario: Esto indica una relación bidireccional con los usuarios en donde se indica que una "transaction"
   # pertenece a un "user"
   belongs_to :user
   # Comentario: Esto indica una relacion con los detalles de transacciones en donde una "transaction" tiene o puede tener 
-  # muchos "transaction_detail"
-  has_many :transaction_detail
+  # muchos "transaction_detail", se agrega el inverse_of para que reconozca la relacion bidireccional hecha de una forma no estandar con transaction_details
+  has_many :transaction_details, inverse_of: 'order'
+
+  # Comentario
+  accepts_nested_attributes_for :transaction_details, reject_if: :all_blank, allow_destroy: true
+
+   # Comentario: Si el campo de user_id va con un valor nulo (nil) se le asigna el valor del id del usuario actual
+   before_save :set_user_id
+
+   # Comentario: Este es el método que se llama en el before_save(antes de guardar)
+   def set_user_id
+     self.user_id = current_user.id if self.user_id.nil?
+   end
 end
--- app/models/transaction_detail.rb
+++ app/models/transaction_detail.rb
@@ -1,7 +1,9 @@
 class TransactionDetail < ApplicationRecord
-  # Comentario: Esto indica una relación con las transacciones en donde se indica que un "transaction_detail"
-  # pertenece a una "transaction"
-  belongs_to :transaction
+  # Comentario: Esto indica una relación bidireccional con las transacciones en donde se indica que un "transaction_detail"
+  # pertenece a una "transaction", esta parte deberia lucir asi 'belongs_to :transaction' pero 'transaction' es una 
+  # palabra reservada de rails , por lo que se hace una declaracion diferente indicandole que se relaciona al modelo de transacciones pero
+  # la relacion estara bajo el nombre de order
+  belongs_to :order, foreign_key: "transaction_id", class_name: "Transaction"
   # Comentario: Esto indica una relación con los productos en donde se indica que un "transaction_detail"
   # pertenece a un "product"
   belongs_to :product

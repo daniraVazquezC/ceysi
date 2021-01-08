8452e22803f393ba3664ae6b8ec7af8fa67a70ca
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-08 13:20:24 -0600

    modificar select de productos

--- app/javascript/packs/custom/general_script.js
+++ app/javascript/packs/custom/general_script.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import 'select2'
+import 'select2/dist/css/select2.css'
 
 $(document).on('turbolinks:load', function () {
 
@@ -21,8 +23,8 @@ $(document).on('turbolinks:load', function () {
     }    
   });
 
-  
+  $('#new_sale').on('cocoon:after-insert', function() {
+    $('.select-product').select2()
+  });
 
 });
-
-

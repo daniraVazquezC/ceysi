7b385151b44e3f86ec5e2c6c0647d9bb2b326229
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-09 18:36:36 -0600

    realizar cambios en el modelo de transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,16 +4,22 @@ class Transaction < ApplicationRecord
   belongs_to :user
   # Comentario: Esto indica una relacion con los detalles de transacciones en donde una "transaction" tiene o puede tener 
   # muchos "transaction_detail", se agrega el inverse_of para que reconozca la relacion bidireccional hecha de una forma no estandar con transaction_details
-  has_many :transaction_details, inverse_of: 'order'
+  has_many :transaction_details, inverse_of: 'order', dependent: :destroy
 
-  # Comentario
+  # Comentario: Con esto se le indica al modelo que se van a aceptar atributos anidados que pertenecen al modelo de transaction_details
   accepts_nested_attributes_for :transaction_details, reject_if: :all_blank, allow_destroy: true
 
-   # Comentario: Si el campo de user_id va con un valor nulo (nil) se le asigna el valor del id del usuario actual
-   before_save :set_user_id
 
-   # Comentario: Este es el método que se llama en el before_save(antes de guardar)
-   def set_user_id
-     self.user_id = current_user.id if self.user_id.nil?
-   end
+
+  # TODO : Pendiente de revisar 
+   # Comentario: Si el campo de user_id va con un valor nulo (nil) se le asigna el valor del id del usuario actual
+  before_save :set_user_id
+
+  # Comentario: Este es el método que se llama en el before_save(antes de guardar)
+  def set_user_id
+  #  user_id = current_user.id if user_id.nil?
+  end
+
 end
+
+

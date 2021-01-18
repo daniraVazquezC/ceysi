08fc4bddf1eedd19a923de155018be3e10bc78a8
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-01-17 19:47:50 -0600

    añadir host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'ceysi.herokuapp.com',
+    :host               => 'ceysi.herokuapp.com',
     :user_name            => ENV['SMTP_USER_NAME'],
     :password             => ENV['SMTP_PASSWORD'],
     :authentication       => "plain",
